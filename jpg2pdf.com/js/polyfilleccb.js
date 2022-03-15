5f3d3147ae8fda4a9c671bfdfdffef75755779fe
jpgtopdf <101654562+jpgtopdf@users.noreply.github.com>
2022-03-15 17:15:48 +0100

    Add files via upload

---
+++ jpg2pdf.com/js/polyfilleccb.js
@@ -0,0 +1,46 @@
+
+if (typeof Element !== "undefined") {
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+  }
+
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+      var el = this;
+
+      do {
+        if (el.matches(s)) return el;
+        el = el.parentElement || el.parentNode;
+      } while (el !== null && el.nodeType === 1);
+
+      return null;
+    };
+  }
+}
+Number.isFinite = Number.isFinite || function (value) {
+  return typeof value === 'number' && isFinite(value);
+}
+if (typeof Object.assign != "function") {
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) {
+      "use strict";
+      if (target == null) {
+        throw new TypeError("Cannot convert undefined or null to object");
+      }
+      var to = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+        if (nextSource != null) {
+          for (var nextKey in nextSource) {
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
